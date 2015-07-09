b8c7498b05eb2a0f9fa31bfdbdf4179f8dac1dc4
Michael Garate <mpgarate@gmail.com>
2015-07-09 09:40:44 -0400

    add project bones

---
+++ src/Solver.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 7/9/15
+ * Time: 8:37 AM
+ */
+
+namespace FifteenPuzzle;
+
+class Solver
+{
+    public function sayHello()
+    {
+        return "hello";
+    }
+}
---
+++ test/SolverTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 7/7/15
+ * Time: 10:20 AM
+ */
+
+namespace FifteenPuzzle;
+
+class SolverTest extends \PHPUnit_Framework_TestCase {
+    public function testSayHello()
+    {
+        $solver = new Solver();
+        $this->assertEquals("hello", $solver->sayHello());
+    }
+}
